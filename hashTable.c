44c1d4d0306556e35b5635a5e17b8c2cd9f67d8e
luckybug <luckybug@gmail.com>
2013-07-29 17:27:44 +0800

    open hashTable struct

--- hashTable.c
+++ hashTable.c
@@ -6,21 +6,8 @@
 
 #define CONST_MAX_HASHSIZE  (4096)
 
-typedef struct _hashToken hashToken;
-struct _hashToken
-{
-    hashToken *next;      // for hash table linkage
-    const char *name;
-    void *value;
-};
-
-typedef struct _tableInfo tableInfo;
-struct _tableInfo
-{
-    uint32_t hashSize;
-    hashToken *_table[0];
-};
-
+typedef void *(*allocFunc)(size_t size);
+typedef void (*freeFunc)(void *ptr);
 
 static allocFunc _malloc = malloc;
 static freeFunc _free = free;
@@ -98,35 +85,56 @@ int inline setHtTokenAlloc(allocFunc func1, freeFunc func2)
     return 0;
 }
 
-int initHashTable(void **pHandle, uint32_t hashSize)
+int newHashTable(hashTable **pHashTable, uint32_t hashSize)
 {
     int ret = -1;
     size_t _size;
-    tableInfo *handle;
+    hashTable *handle;
 
     hashSize = realHashSize(hashSize);
-    printf("real hashSize = %d\n", hashSize);
+    //printf("real hashSize = %d\n", hashSize);
     do
     {
-        if (!pHandle || !hashSize)
+        if (!pHashTable || !hashSize)
             break;
 
-        _size = sizeof(tableInfo) + sizeof(hashToken)*hashSize;
-        handle = (tableInfo *)_malloc(_size);
+        _size = sizeof(hashTable) + sizeof(hashToken*)*hashSize;
+        handle = (hashTable *)_malloc(_size);
         if (!handle)
             break;
 
         memset(handle, 0, _size);
         handle->hashSize = hashSize;
+        // xMalloc, xFree could be set after this call
 
-        *pHandle = handle;
+        *pHashTable = handle;
         ret = 0;
     }while(0);
 
     return ret;
 }
 
-static hashToken * findToken(tableInfo *tbl, const char *name, uint32_t delFlag)
+int initHashTable(hashTable *table, uint32_t hashSize)
+{
+    int ret = -1;
+    size_t _size;
+
+    hashSize = realHashSize(hashSize);
+    //printf("real hashSize = %d\n", hashSize);
+    do
+    {
+        if (!table || !hashSize)
+            break;
+
+        table->hashSize = hashSize;
+
+        ret = 0;
+    }while(0);
+
+    return ret;
+}
+
+static hashToken * findToken(hashTable *tbl, const char *name, uint32_t delFlag)
 {
     uint32_t hashValue;
     hashToken *hashPlace = NULL;
@@ -139,53 +147,45 @@ static hashToken * findToken(tableInfo *tbl, const char *name, uint32_t delFlag)
         return NULL;
 
     //ASSERT(hashPlace->name);
-    if (!strcmp(name, hashPlace->name))
-    {
-        if (delFlag)
-        {
+    if (!strcmp(name, hashPlace->name)) {
+        if (delFlag) {
             tbl->_table[hashValue] = hashPlace->next;
         }
-    }
-    else
-    {
+    } else {
         prevPlace = hashPlace;
         hashPlace = hashPlace->next;
         while (hashPlace)
         {
             //ASSERT(hashPlace->name);
-            if (!strcmp(name, hashPlace->name))
+            if (!strcmp(name, hashPlace->name)) {
                 break;
+            }
             prevPlace = hashPlace;
             hashPlace = hashPlace->next;
         }
 
-        if (hashPlace && delFlag)
+        if (hashPlace && delFlag) {
             prevPlace->next = hashPlace->next;
+        }
     }
 
     return hashPlace;
 }
 
-/*
-加入表时，name 做一份 copy, value 只保存指针
-*/
+
 static inline hashToken *createToken(const char *name, void *value)
 {
     hashToken *newItem;
     char *_name;
 
     newItem = _tkmalloc(sizeof(hashToken));
-    if (NULL != newItem)
-    {
+    if (NULL != newItem) {
         _name = _hashStrdup(name);
-        if (_name)
-        {
+        if (_name) {
             newItem->name = _name;
             newItem->value = value;
             newItem->next = NULL;
-        }
-        else
-        {
+        } else {
             _tkfree(newItem);
             newItem = NULL;
         }
@@ -208,31 +208,28 @@ static inline int deleteToken(hashToken *item, void **retval)
     return 0;
 }
 
-int setHashValue(void *handle, const char *name, void *value, void **retval)
+int setHashValue(hashTable *handle, const char *name, void *value, void **retval)
 {
     int ret = -1;
     uint32_t hashValue;
     hashToken *hashPlace = NULL;
     hashToken *newToken = NULL;
-    tableInfo *tbl = (tableInfo *)handle;
+    hashTable *tbl = (hashTable *)handle;
 
     if (retval)
         *retval = NULL;
 
     do
     {   // add '!value' case, 2008-7-3 gaokp
-        if (!handle || !name)
+        if (!handle || !name || !value) {
             break;
+        }
 
         newToken = findToken(handle, name, 0);
-        if (!newToken)
-        {
+        if (!newToken) {
             newToken = createToken(name, value);
-        }
-        else
-        {
-            if (retval)
-            {
+        } else {
+            if (retval) {
                 *retval = newToken->value;
             }
             newToken->value = value;
@@ -240,24 +237,27 @@ int setHashValue(void *handle, const char *name, void *value, void **retval)
             break;
         }
 
-        if (NULL == newToken)
+        if (NULL == newToken) {
             break;
+        }
 
         hashValue = djb2_hash(name, -1) & (tbl->hashSize - 1);
         hashPlace = tbl->_table[hashValue];
 
         while (hashPlace)
         {
-            if (hashPlace->next)
+            if (hashPlace->next) {
                 hashPlace = hashPlace->next;
-            else
+            } else {
                 break;
+            }
         }
 
-        if (!hashPlace)
+        if (!hashPlace) {
             tbl->_table[hashValue] = newToken;
-        else
+        } else {
             hashPlace->next = newToken;
+        }
 
         ret = 0;
     }while(0);
@@ -265,20 +265,22 @@ int setHashValue(void *handle, const char *name, void *value, void **retval)
     return ret;
 }
 
-int getHashValue(void *handle, const char *name, void **retval)
+int getHashValue(hashTable *handle, const char *name, void **retval)
 {
     int ret = -1;
     hashToken *newToken = NULL;
-    tableInfo *tbl = (tableInfo *)handle;
+    hashTable *tbl = (hashTable *)handle;
 
     do
     {
-        if (!handle || !name || !retval)
+        if (!handle || !name || !retval) {
             break;
+        }
 
         newToken = findToken(handle, name, 0);
-        if (!newToken)
+        if (!newToken) {
             break;
+        }
 
         *retval = newToken->value;
         ret = 0;
@@ -287,20 +289,22 @@ int getHashValue(void *handle, const char *name, void **retval)
     return ret;
 }
 
-int delHashValue(void *handle, const char *name, void **retval)
+int delHashValue(hashTable *handle, const char *name, void **retval)
 {
     int ret = -1;
     hashToken *newToken = NULL;
-    tableInfo *tbl = (tableInfo *)handle;
+    hashTable *tbl = (hashTable *)handle;
 
     do
     {
-        if (!handle || !name)
+        if (!handle || !name) {
             break;
+        }
 
         newToken = findToken(handle, name, 1);
-        if (!newToken)
+        if (!newToken) {
             break;
+        }
 
         ret = deleteToken(newToken, retval);
     }while(0);
@@ -308,16 +312,17 @@ int delHashValue(void *handle, const char *name, void **retval)
     return ret;
 }
 
-int iterHashTable(void *handle, iterFunc callback, void *param)
+int iterHashTable(hashTable *handle, iterFunc callback, void *param)
 {
     int i;
     int level;
     hashToken *node = NULL;
     iterCtrl ctrl = ITERCTRL_CONTINUE;
-    tableInfo *tbl = (tableInfo *)handle;
+    hashTable *tbl = (hashTable *)handle;
 
-    if (!handle || !callback)
+    if (!handle || !callback) {
         return -1;
+    }
 
     for (i=0; i<tbl->hashSize; i++)
     {
@@ -328,16 +333,16 @@ int iterHashTable(void *handle, iterFunc callback, void *param)
         {
             ctrl = callback(node->name, node->value, param);
 
-            if (ITERCTRL_BREAK == ctrl)
+            if (ITERCTRL_BREAK == ctrl) {
                 goto end;
+            }
 
-            if (node->next)
-            {
+            if (node->next) {
                 level++;
                 node = node->next;
-            }
-            else
+            } else {
                 break;
+            }
         }
     }
 
@@ -345,12 +350,12 @@ end:
     return (ITERCTRL_BREAK == ctrl)? -1 : 0;
 }
 
-void dumpHashTable(void *handle)
+void dumpHashTable(hashTable *handle)
 {
     int i;
     int level;
     hashToken *node = NULL;
-    tableInfo *tbl = (tableInfo *)handle;
+    hashTable *tbl = (hashTable *)handle;
 
     if (!handle)
         return;
@@ -365,13 +370,12 @@ void dumpHashTable(void *handle)
             printf("%s(H:%d,L:%d):\n", node->name, i, level);
             printf("%p\n", node->value);
 
-            if (node->next)
-            {
+            if (node->next) {
                 level++;
                 node = node->next;
-            }
-            else
+            } else {
                 break;
+            }
         }
 
     }
--- include/hashTable.h
+++ include/hashTable.h
@@ -1,3 +1,4 @@
+// hash table of string to string
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
@@ -7,7 +8,45 @@
 extern "C"
 {
 #endif
-    
+
+#define DEFINE_HASH_TABLE(name, row_num)  \
+hashTable ##name; \
+hashToken *##name_rows[row_num];
+
+typedef struct _hashToken hashToken;
+struct _hashToken
+{
+    hashToken *next;      // for hash table linkage
+    const char *name;
+    void *value;
+};
+
+typedef struct _tableInfo tableInfo;
+typedef struct _tableInfo hashTable;
+
+struct _tableInfo
+{
+    uint32_t reserved;
+    uint32_t hashSize;      // DO NOT modify
+#if 0
+    void *(*xMalloc)(size_t);  /* malloc() function to use */
+    void (*xFree)(void *);     /* free() function to use */
+
+    void *(*tkMalloc)(size_t);
+    void (*tkFree)(void *);
+#endif
+    hashToken *_table[0];
+};
+
+// NOTICE: use free() to de-allocate the object created by 'newHashTable'
+int newHashTable(hashTable **pHashTable, uint32_t hashSize);
+// 'initHashTable' is for initializing pre-allocated hast table
+int initHashTable(hashTable *table, uint32_t hashSize);
+
+int setHashValue(hashTable *table, const char *name, void *value, void **retval);
+int getHashValue(hashTable *table, const char *name, void **retval);
+int delHashValue(hashTable *table, const char *name, void **retval);
+
 typedef enum _iterCtrl iterCtrl;
 enum _iterCtrl
 {
@@ -17,31 +56,9 @@ enum _iterCtrl
 
 typedef iterCtrl (*iterFunc)(const char *tokenName, void *tokenValue, void *param);
 typedef int (*diffFunc)(void *value1, void *value2);
+int iterHashTable(hashTable *table, iterFunc callback, void *param);
 
-typedef void *(*allocFunc)(size_t size);
-typedef void (*freeFunc)(void *ptr);
-
-int inline setHtStringAlloc(allocFunc func1, freeFunc func2);
-
-int initHashTable(void **pHandle, uint32_t hashSize);
-
-int setHashValue(void *handle, const char *name, void *value, void **retval);
-int getHashValue(void *handle, const char *name, void **retval);
-int delHashValue(void *handle, const char *name, void **retval);
-
-int iterHashTable(void *handle, iterFunc callback, void *param);
-void dumpHashTable(void *handle);
-
-/*
-inline int modifyValue(char * name, const char * value);
-inline int lookupValue(const char *name, char *value, uint32_t maxlen);
-
-
-inline int delConfigToken(const char * name);
-
-void tableHealth();
-*/
-
+void dumpHashTable(hashTable *table);
 
 #if defined(__cplusplus)
 };
